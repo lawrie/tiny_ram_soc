5cdf3882cc093e860f52b33e285b220d305c6357
lawrie <lawrie.griffiths@ntlworld.com>
2018-10-22 18:13:36 +0100

    Blink led

--- examples/game_menu/main.c
+++ examples/game_menu/main.c
@@ -3,11 +3,20 @@
 #include <uart/uart.h>
 
 #define reg_uart_clkdiv (*(volatile uint32_t*)0x02000004)
+#define reg_leds (*(volatile uint32_t*)0x03000000)
 
 void main() {
 
   reg_uart_clkdiv = 139;
 
   print("Hello World!\n");
+
+    // blink the user LED
+    uint32_t led_timer = 0;
+       
+    while (1) {
+        reg_leds = led_timer >> 16;
+        led_timer = led_timer + 1;
+    } 
 }
 
